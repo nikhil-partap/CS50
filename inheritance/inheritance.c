c4ecaf5a7da25872a062ce12d304a9448743fa6b
Nikhil Partap Singh <nikhilpartapsingh88@gmail.com>
2025-07-05 23:04:06 +0530

    Update inheritance.c

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -33,13 +33,20 @@ int main(void)
 
     // Free memory
     free_family(p);
+    
 }
 
 // Create a new individual with `generations`
 person *create_family(int generations)
 {
     // TODO: Allocate memory for new person
-
+    person *new_person = malloc(sizeof(person));
+    if (new_person == NULL){
+        free(new_person);
+        return NULL;
+    }
+    
+    
     // If there are still generations left to create
     if (generations > 1)
     {
@@ -48,31 +55,50 @@ person *create_family(int generations)
         person *parent1 = create_family(generations - 1);
 
         // TODO: Set parent pointers for current person
+        new_person->parents[0] = parent0;
+        new_person->parents[1] = parent1;
 
         // TODO: Randomly assign current person's alleles based on the alleles of their parents
+        new_person->alleles[0] = parent0->alleles[random() % 2];
+        new_person->alleles[1] = parent1->alleles[random() % 2];
+        
     }
 
     // If there are no generations left to create
     else
     {
+        
         // TODO: Set parent pointers to NULL
+        new_person->parents[0] = NULL;
+        new_person->parents[1] = NULL;
 
         // TODO: Randomly assign alleles
+        new_person->alleles[0] = random_allele();
+        new_person->alleles[1] = random_allele();
+
+
     }
 
     // TODO: Return newly created person
-    return NULL;
+    
+    return new_person;
 }
 
 // Free `p` and all ancestors of `p`.
 void free_family(person *p)
 {
     // TODO: Handle base case
+    if (p == NULL)
+    {
+        return ;
+    }
 
     // TODO: Free parents recursively
+    free_family(p->parents[0]);
+    free_family(p->parents[1]);
 
     // TODO: Free child
-}
+    free(p);}
 
 // Print each family member and their alleles.
 void print_family(person *p, int generation)
